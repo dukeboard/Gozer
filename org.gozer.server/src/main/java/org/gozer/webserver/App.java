05fda3735bbc38bd356abba53e0153ec5829b08a
François Fouquet <fouquet.f@gmail.com>
2012-03-28 20:46:06 +0200

    init gozer

---
+++ org.gozer.server/src/main/java/org/gozer/webserver/App.java
@@ -0,0 +1,16 @@
+package org.gozer.webserver;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: duke
+ * Date: 28/03/12
+ * Time: 20:45
+ */
+public class App {
+    
+    public void main(String[] args){
+
+        
+
+    }
+}
