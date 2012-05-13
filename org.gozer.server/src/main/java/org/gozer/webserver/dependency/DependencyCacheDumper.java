890eef5b730cd22fa0397ba9539f93d141cad941
sebbrousse <seb.brousse@gmail.com>
2012-05-13 20:03:02 +0200

    creating a visitor to store new node in cache and unit test

--- org.gozer.server/src/main/java/org/gozer/webserver/dependency/DependencyCacheDumper.java
+++ org.gozer.server/src/main/java/org/gozer/webserver/dependency/DependencyCacheDumper.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.sonatype.aether.graph.DependencyNode;
 import org.sonatype.aether.graph.DependencyVisitor;
+import org.sonatype.aether.util.graph.AbstractDepthFirstNodeListGenerator;
 
 import java.io.File;
 
@@ -13,18 +14,30 @@ import java.io.File;
  * User: sebastien
  * Date: 10/05/12
  */
-public class DependencyCacheDumper implements DependencyVisitor {
+public class DependencyCacheDumper extends AbstractDepthFirstNodeListGenerator {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DependencyCacheDumper.class);
 
     private DependencyCache cache;
 
     public DependencyCacheDumper() {
-        cache = new DependencyCache();
+        cache = DependencyCache.getInstance();
     }
 
     @Override
     public boolean visitEnter(DependencyNode node) {
+        LOGGER.debug("entering {}", node.getDependency());
+
+        if (!setVisited(node)) {
+            return false;
+        }
+
+        if ( node.getDependency() != null )
+        {
+            nodes.add( node );
+        }
+
+
         LOGGER.debug("entering {}", node.getDependency());
         cache.put(node.getDependency().toString(), null);
         return true;  //To change body of implemented methods use File | Settings | File Templates.
